7e2769c67ad2e49a6727f77f9748f9980fc31146
Amenov <amenov.abdulsalam@gmail.com>
2021-07-13 21:53:16 +0600

    first commit

---
+++ only.js
@@ -0,0 +1,38 @@
+const flat = require('flat');
+
+module.exports =
+  (object) =>
+  (...keys) => {
+    if (Array.isArray(keys[0])) keys = keys[0];
+
+    const flatObject = flat.flatten(object);
+    const flatKeys = [];
+
+    function flattenKeys(keys, prevKey = '') {
+      for (const key of keys) {
+        if (typeof key === 'string') {
+          flatKeys.push(prevKey ? `${prevKey}.${key}` : key);
+        } else {
+          for (const [_key, _keys] of Object.entries(key)) {
+            flattenKeys(_keys, prevKey ? `${prevKey}.${_key}` : _key);
+          }
+        }
+      }
+    }
+
+    flattenKeys(keys);
+
+    const siftedFlatObject = {};
+
+    for (const flatKey of flatKeys) {
+      const value = flatObject[flatKey];
+
+      if (value !== undefined) {
+        siftedFlatObject[flatKey] = value;
+      }
+    }
+
+    const siftedBody = flat.unflatten(siftedFlatObject);
+
+    return Object.keys(siftedBody).length ? siftedBody : null;
+  };
