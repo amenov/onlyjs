1e6c3ccbb5e6c52d8e12aa644cc241414463a5e8
Amenov <amenov.abdulsalam@gmail.com>
2021-07-13 21:56:08 +0600

    2156

--- only.js
+++ only.js
@@ -1,38 +1,36 @@
 const flat = require('flat');
 
-module.exports =
-  (object) =>
-  (...keys) => {
-    if (Array.isArray(keys[0])) keys = keys[0];
+module.exports = (object, ...keys) => {
+  if (Array.isArray(keys[0])) keys = keys[0];
 
-    const flatObject = flat.flatten(object);
-    const flatKeys = [];
+  const flatObject = flat.flatten(object);
+  const flatKeys = [];
 
-    function flattenKeys(keys, prevKey = '') {
-      for (const key of keys) {
-        if (typeof key === 'string') {
-          flatKeys.push(prevKey ? `${prevKey}.${key}` : key);
-        } else {
-          for (const [_key, _keys] of Object.entries(key)) {
-            flattenKeys(_keys, prevKey ? `${prevKey}.${_key}` : _key);
-          }
+  function flattenKeys(keys, prevKey = '') {
+    for (const key of keys) {
+      if (typeof key === 'string') {
+        flatKeys.push(prevKey ? `${prevKey}.${key}` : key);
+      } else {
+        for (const [_key, _keys] of Object.entries(key)) {
+          flattenKeys(_keys, prevKey ? `${prevKey}.${_key}` : _key);
         }
       }
     }
+  }
 
-    flattenKeys(keys);
+  flattenKeys(keys);
 
-    const siftedFlatObject = {};
+  const siftedFlatObject = {};
 
-    for (const flatKey of flatKeys) {
-      const value = flatObject[flatKey];
+  for (const flatKey of flatKeys) {
+    const value = flatObject[flatKey];
 
-      if (value !== undefined) {
-        siftedFlatObject[flatKey] = value;
-      }
+    if (value !== undefined) {
+      siftedFlatObject[flatKey] = value;
     }
+  }
 
-    const siftedBody = flat.unflatten(siftedFlatObject);
+  const siftedBody = flat.unflatten(siftedFlatObject);
 
-    return Object.keys(siftedBody).length ? siftedBody : null;
-  };
+  return Object.keys(siftedBody).length ? siftedBody : null;
+};
